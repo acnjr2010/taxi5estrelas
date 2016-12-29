1784bc31f310f7fc4f208325dafaa2aed5df8dc0
Junior <nog.junior84@gmail.com>
2016-12-29 16:09:59 -0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :welcome
   root "welcome#index"
+
+  get "/aplicativo", controller: 'welcome', action: 'aplicativo'
+  get "/promocao", controller: 'welcome', action: 'promocao'
+  get "/contato", controller: 'welcome', action: 'contato'
 end
