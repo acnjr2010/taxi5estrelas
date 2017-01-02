9a09357abe169fa8a7fe5b18d50de4dc53ac6d1a
Junior <nog.junior84@gmail.com>
2017-01-02 12:23:12 -0200

    Mensagem Administrativo

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'paperclip', '~> 5.1'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.3', '>= 3.3.1'
 gem 'rails-i18n', '~> 5.0.0.beta1'
+gem 'will_paginate', '~> 3.1'
+gem 'ckeditor', '~> 4.2', '>= 4.2.2'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
     @apps = Aplicativo.all.count
     @promocoes = Promocao.all.count
   end
+
+  def home_admin
+  end
+
+  def aplicativo_admin
+  end
+
+  def promocao_admin
+  end
+
+  def mensagens_admin
+    @messages = Message.all.paginate(page: params[:page], per_page: 10).order('created_at DESC')
+  end
 end
