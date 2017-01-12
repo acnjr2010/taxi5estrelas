edcb6bcf757369bb9ce0c9aa9720e093209172b5
Junior <nog.junior84@gmail.com>
2017-01-12 17:03:54 -0200

    Verificação Telefone

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   validates :name, :email, :phone, :message, presence: true
   validates :email, email_format: { message: "Email inválido" }
+  validates_numericality_of :phone, :on => :create
 end
